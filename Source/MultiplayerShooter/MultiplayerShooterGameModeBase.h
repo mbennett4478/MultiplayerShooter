1a56eb7b0c0d0d06474f780b24c1cbf73831b875
Matthew Bennett <mbennett@huskers.unl.edu>
2018-04-09 21:49:36 -0700

    initial project

---
+++ Source/MultiplayerShooter/MultiplayerShooterGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MultiplayerShooterGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MULTIPLAYERSHOOTER_API AMultiplayerShooterGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
